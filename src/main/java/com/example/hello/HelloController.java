ea5b8bc5b8624c9d600a43ad6a13cf824c2a888a
katayama <tattu.0310@gmail.com>
2022-09-25 17:11:55 +0900

    ✨ feat/return json

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,13 +1,36 @@
 package com.example.hello;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import student.Student;
+
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
 
+    @GetMapping("/hello/list")
+    public List<String> helloList() {
+        return List.of("Hello", "World");
+    }
+
+    @GetMapping("/student")
+    public List<Student> stu() {
+        return List.of(
+                new Student(
+                        1L,
+                        "James Bond",
+                        LocalDate.of(2000, Month.JUNE, 5),
+                        "gmail@gnail.com",
+                        30));
+
+    }
 }
